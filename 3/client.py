f83e8b06158fe9844b8b8502c9a362680a3fd84c
andrey <fish@dedlyfish.com>
2018-01-22 18:01:11 +1100

    multithreaded server, write-only client

--- 3/client.py
+++ 3/client.py
@@ -2,39 +2,31 @@ from socket import *
 from message import JimMessage
 from response import JimResponse
 
+
 class JimClient:
-    def __init__(self, host, port):
+    def __init__(self, host, port, user=None, password=None):
         self.host, self.port = host, port
-        self.user, self.password = 'guest', ''
+        self.user = user or 'guest'
+        self.password = password or ''
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+        self.msg = JimMessage()
+        self.rsp = JimResponse()
 
-    def __connect(self):
-        try:
-            self.socket = socket(AF_INET, SOCK_STREAM)
-            self.socket.connect((self.host, self.port))
-        except:
-            print("Can't connect to {}:{}".format(self.host, self.port))
-
-    def set_account(self, user, password):
-        self.user = user
-        self.password = password
+    def disconnect(self):
+        self.socket.close()
 
     def send_presence(self, status=''):
-        msg = JimMessage()
-        rsp = JimResponse()
         if status == '':
-            msg.compose('presence', user={'account_name': self.user, 'status': status})
+            self.msg.compose('presence', user={'account_name': self.user, 'status': status})
         else:
-            msg.compose('presence', type='status', user={'account_name': self.user, 'status': status})
-        self.__connect()
-        if msg.send_message(self.socket):
-            return rsp.read_response(self.socket)
+            self.msg.compose('presence', type='status', user={'account_name': self.user, 'status': status})
+        if self.msg.send_message(self.socket):
+            return self.rsp.read_response(self.socket)
         return None
 
     def send_message(self, user, message):
-        msg = JimMessage()
-        rsp = JimResponse()
-        msg.compose('msg', msg_to=user, msg_from=self.user, msg=message)
-        self.__connect()
-        if msg.send_message(self.socket):
-            return rsp.read_response(self.socket)
+        self.msg.compose('msg', msg_to=user, msg_from=self.user, msg=message)
+        if self.msg.send_message(self.socket):
+            return self.rsp.read_response(self.socket)
         return None
--- 3/server.py
+++ 3/server.py
@@ -2,16 +2,47 @@ from socketserver import *
 from message import JimMessage
 from response import JimResponse
 
+clients = []
+
+def send_all(msg):
+    for sock in clients:
+        msg.send_message(sock)
+
+
 class JimTCPHandler(StreamRequestHandler):
-    def handle(self):
-        msg = JimMessage()
+    def handle_input(self, input_msg):
         rsp = JimResponse()
-        data = msg.read_message(self.request)
-        print(data)
-        rsp.compose(200, 'Test')
-        rsp.send_response(self.request)
+        if input_msg['action'] == 'presence':
+            rsp.compose(200, 'presence accepted')
+        elif input_msg['action'] == 'msg':
+            rsp.compose(200, 'message accepted')
+        else:
+            rsp.compose(400, 'incorrect request')
+        return rsp
+
+    def handle(self):
+        clients.append(self.request)
+        print('connected {}'.format(len(clients)))
+        while True:
+            msg = JimMessage()
+            rsp = JimResponse()
+            try:
+                data = msg.read_message(self.request)
+                print(data)
+                self.handle_input(data).send_response(self.request)
+            except:
+                break
+            del msg
+            del rsp
+        print('client disconnected')
+        clients.remove(self.request)
+
+
+class ThreadedTCPServer(ThreadingMixIn, TCPServer):
+    pass
+
 
 if __name__ == '__main__':
-    server = TCPServer(('127.0.0.1', 7777), JimTCPHandler)
+    server = ThreadedTCPServer(('127.0.0.1', 7777), JimTCPHandler)
     print('started server')
     server.serve_forever()
